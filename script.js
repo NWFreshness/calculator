10e44cf174dbc2dad234e5fbf6318bc671594242
Tyler Mayfield <tyler.mayfield23@gmail.com>
2022-03-11 15:46:47 -0800

    users can click number and change display

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ var operators = {
 function operate(operator, num1, num2){
     return operators[operator](num1, num2);
 }
+
+let numbersArray = [];
+
+const display = document.querySelector('.display');
+const numbers = document.querySelector('.numbers').children;
+const functions = document.querySelector('.functions').children;
+
+
+Array.from(numbers).forEach(e=>{
+    e.addEventListener('click',  ()=>{
+        if(e.innerText != 'Clear'){
+            numbersArray.push(e.innerText);
+            display.textContent = e.innerText;
+        }
+    });
+})
+
+
+// console.log(numbers);
+// console.log(functions)
+
