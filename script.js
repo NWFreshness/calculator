7bbbffeac6582e8fff0b4876ee53ccbde98c3910
Tyler Mayfield <tyler.mayfield23@gmail.com>
2022-03-11 16:20:32 -0800

    Calculator functioning

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function operate(operator, num1, num2){
 }
 
 let numbersArray = [];
+let operator = '';
 
 const display = document.querySelector('.display');
 const numbers = document.querySelector('.numbers').children;
@@ -29,10 +30,35 @@ Array.from(numbers).forEach(e=>{
         if(e.innerText != 'Clear'){
             numbersArray.push(e.innerText);
             display.textContent = e.innerText;
+        }else{
+            numbersArray = [];
+            operator = '';
+            display.textContent = '0'
         }
     });
 })
 
+Array.from(functions).forEach(e=>{
+    e.addEventListener('click', ()=>{
+        if(e.innerText === '='){
+            display.textContent = operate(operator, +numbersArray[0], +numbersArray[1]);
+        }
+        if(numbersArray[0] != undefined && numbersArray[1] != undefined){
+            if(e.innerText === '+' || e.innerText === '-' || e.innerText === '*' || e.innerText === '/'){
+                
+                display.textContent = operate(operator, +numbersArray[0], +numbersArray[1])
+                numbersArray[0] = operate(operator, +numbersArray[0], +numbersArray[1]);
+                operator = e.innerText;
+                numbersArray.pop();
+                
+            }
+        }
+        else{
+            console.log(e.innerText);
+            operator = e.innerText;
+        }
+    })
+})
 
 // console.log(numbers);
 // console.log(functions)
